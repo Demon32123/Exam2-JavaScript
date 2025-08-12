ba8e9cfe651c9f2ef94fb941b26fda40a6d2643d
Dmitry Sheiko <dmitry.sheiko@mail.ru>
2025-08-12 23:16:21 +0300

    feat: the code works and everything seems to be done but there is a strange error and delete one comment

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -2,9 +2,4 @@ export interface HtmlElements {
   container: HTMLElement | null;
   button: HTMLButtonElement | null;
   inputTask: HTMLInputElement | null;
-  //   container: document.getElementById("container") as HTMLElement;
-  //   button: document.getElementById("addButton") as HTMLButtonElement | null;
-  //   inputTask: document.getElementById(
-  //     "textInput"
-  //   ) as HTMLInputElement | null
 }
